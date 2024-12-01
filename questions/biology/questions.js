5e46633361f486e0d8a76584ebd406b118dcbeda
Mahmudul Islam Mahin <144619256+itzMahin06@users.noreply.github.com>
2024-12-01 22:13:04 +0600

    Delete questions directory

--- questions/biology/questions.js
+++
@@ -1,1 +0,0 @@
-const mcqQuestions = [   { question: " কোনটি দেহকোষ নয়? ", options: ["স্নায়ু কোষ", "রক্তকণিকা", "শুক্রাণু", "পাতার কোষ"], answer: 3 },   { question: "What is H2O commonly known as?", options: ["Salt", "Oxygen", "Water", "Hydrogen"], answer: 2 }, ];
--- questions/math/questions.js
+++
@@ -1,5 +0,0 @@
-// File: ./questions/math/questions.js
-export const mcqQuestions = [
-  { question: "What is 2 + 2?", options: ["3", "4", "5", "6"], answer: 1 },
-  { question: "What is the capital of France?", options: ["Berlin", "London", "Paris", "Rome"], answer: 2 },
-];
