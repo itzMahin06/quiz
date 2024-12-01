ffa6c53008638f7e50d42abf7fb26085632ac4e7
Mahmudul Islam Mahin <144619256+itzMahin06@users.noreply.github.com>
2024-12-01 21:46:23 +0600

    Delete question directory

--- question/bio1.js
+++
@@ -1,6 +0,0 @@
-// Sample Questions
-    const mcqQuestions = [
-      { question: "What is 2 + 2?", options: ["1", "2", "3", "4"], answer: 3 },
-      { question: "What is the capital of France?", options: ["Paris", "Rome", "Madrid", "Berlin"], answer: 0 },
-      // Add more questions as needed
-    ];
